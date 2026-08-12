5202d0ab32aa432ea6fb3ed8d821dd703de05fb0
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-08-11 23:13:40 -0300

    Add edge-to-edge support and import for Compose

--- app/src/main/java/com/rhyan57/rcst/MainActivity.kt
+++ app/src/main/java/com/rhyan57/rcst/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.rhyan57.rcst.ui.components.CrashScreen
